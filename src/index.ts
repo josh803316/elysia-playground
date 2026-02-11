5b37103979cd4da8269fbe50b1a9ff3c5c79a0e8
Josh Nisenson <josh803316@gmail.com>
2026-02-11 17:09:10 -0300

    chore: Revise vercel deploy elysia initial setup

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-// Vercel Elysia integration: import from 'elysia' and export an Elysia instance
-// See: https://elysiajs.com/integrations/vercel
-import { Elysia } from 'elysia';
-import app from '../server/src/index';
-
-export default app;
-
-
