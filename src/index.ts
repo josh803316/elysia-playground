a100a254ce203c3908d2b7e11bcb483f13c5242f
Josh Nisenson <josh803316@gmail.com>
2026-02-11 16:10:36 -0300

    chore: Revise vercel deploy elysia entry

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-import app from "../server/src/index";
+// Vercel Elysia integration: import from 'elysia' and export an Elysia instance
+// See: https://elysiajs.com/integrations/vercel
+import { Elysia } from 'elysia';
+import app from '../server/src/index';
 
-// Re-export the Elysia app so Vercel can auto-detect the entrypoint.
-// Vercel searches for src/index.{js,ts} by default for Elysia backends.
 export default app;
+
 
